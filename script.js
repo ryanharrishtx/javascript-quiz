0c0f2913607c0286b1e7887f75d31928b381c252
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2023-09-12 05:17:23 -0500

    initial commit. have onclick event working to start game

---
+++ script.js
@@ -0,0 +1,6 @@
+var startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", function() {
+    document.getElementById("start-screen").style.display = "none";
+    document.getElementById("quiz").style.display = "flex";
+    });
