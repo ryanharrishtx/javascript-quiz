9cd0490e3bee84767342ecf567c26f9d3d77eb91
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2023-09-16 21:08:43 -0500

    trying again

--- assets/script.js
+++ assets/script.js
@@ -1,156 +1,156 @@
 const Questions = [
-    {
-      q: "Inside which HTML element do we put the JavaScript?",
-      a: [
-        { text: "<javascript>", isCorrect: false },
-        { text: "<js>", isCorrect: false },
-        { text: "<script>", isCorrect: true },
-        { text: "<scripting>", isCorrect: false },
-      ],
-    },
-    {
-      q: "What is the correct syntax for referring to an external script called `xxx.js`?",
-      a: [
-        { text: `<script src="xxx.js">`, isCorrect: true },
-        { text: `<script name="xxx.js">`, isCorrect: false },
-        { text: `<script href="xxx.js">`, isCorrect: false },
-      ],
-    },
-    {
-      q: "How do you create a function?",
-      a: [
-        { text: `function:myFunction()`, isCorrect: false },
-        { text: `function=myFunction()`, isCorrect: false },
-        { text: `function=myFunction()`, isCorrect: false },
-        { text: `function myFunction()`, isCorrect: true },
-      ],
-    },
-    {
-      q: `How do you call a function named "myFunction"?`,
-      a: [
-        { text: `myFunction()`, isCorrect: true },
-        { text: `call myFunction()`, isCorrect: false },
-        { text: `call function myFunction`, isCorrect: false },
-        { text: `Call.myFunction()`, isCorrect: false },
-      ],
-    },
-    {
-      q: "How do you write an IF statement in JavaScript?",
-      a: [
-        { text: `if i = 5 then`, isCorrect: false },
-        { text: `if (i === 5)`, isCorrect: true },
-        { text: `if i == 5 then`, isCorrect: false },
-        { text: `if i = 5 then`, isCorrect: false },
-      ],
-    },
-  ];
-  
-  let currentQuestion = 0;
-  let score = 0;
-  let seconds = 60;
-  let initialTime = 60;
-  let timerInterval;
-  
-  var startButton = document.getElementById("start-button");
-  
-  startButton.addEventListener("click", function () {
-    document.getElementById("start-screen").style.display = "none";
-    document.getElementById("quiz").style.display = "flex";
-    timer(seconds);
-  });
-  
-  function displayQuestion() {
-    const question = document.getElementById("question");
-    const quiz = document.getElementById("answers");
-  
-    question.innerHTML = Questions[currentQuestion].q;
-    answers.innerHTML = ""; // clears out previous answers
-  
-    for (let i = 0; i < Questions[currentQuestion].a.length; i++) {
-      const answerButton = document.createElement("button");
-      answerButton.textContent = Questions[currentQuestion].a[i].text;
-  
-      answerButton.addEventListener("click", function () {
-        checkAnswer(i);
-      });
-      quiz.appendChild(answerButton);
-    }
-  }
-  displayQuestion();
-  
-  function loadScore() {
-    const scoreDiv = document.getElementById("score");
-    const finalScore = document.getElementById("final-score");
-    scoreDiv.style.display = "flex";
-    scoreMessage = `${score} out of ${Questions.length}`;
-    finalScore.innerHTML = scoreMessage;
-  }
+  {
+    q: "Inside which HTML element do we put the JavaScript?",
+    a: [
+      { text: "<javascript>", isCorrect: false },
+      { text: "<js>", isCorrect: false },
+      { text: "<script>", isCorrect: true },
+      { text: "<scripting>", isCorrect: false },
+    ],
+  },
+  {
+    q: "What is the correct syntax for referring to an external script called `xxx.js`?",
+    a: [
+      { text: `<script src="xxx.js">`, isCorrect: true },
+      { text: `<script name="xxx.js">`, isCorrect: false },
+      { text: `<script href="xxx.js">`, isCorrect: false },
+    ],
+  },
+  {
+    q: "How do you create a function?",
+    a: [
+      { text: `function:myFunction()`, isCorrect: false },
+      { text: `function=myFunction()`, isCorrect: false },
+      { text: `function=myFunction()`, isCorrect: false },
+      { text: `function myFunction()`, isCorrect: true },
+    ],
+  },
+  {
+    q: `How do you call a function named "myFunction"?`,
+    a: [
+      { text: `myFunction()`, isCorrect: true },
+      { text: `call myFunction()`, isCorrect: false },
+      { text: `call function myFunction`, isCorrect: false },
+      { text: `Call.myFunction()`, isCorrect: false },
+    ],
+  },
+  {
+    q: "How do you write an IF statement in JavaScript?",
+    a: [
+      { text: `if i = 5 then`, isCorrect: false },
+      { text: `if (i === 5)`, isCorrect: true },
+      { text: `if i == 5 then`, isCorrect: false },
+      { text: `if i = 5 then`, isCorrect: false },
+    ],
+  },
+];
 
-  function highScore() {
-    const highScoreDiv = document.getElementById("high-score");
-    const highScore = document.getElementById("high-score");
-  }
-  
-  function timer(seconds) {
-    const timerDiv = document.getElementById("timer");
-    let counter = seconds;
-  
-    timerInterval = setInterval(() => {
-      timerDiv.innerText = counter;
-      counter--;
-      if (counter < 10) {
-        document.getElementById("timer").style.color = "red";
-      }
+let currentQuestion = 0;
+let score = 0;
+let seconds = 60;
+let initialTime = 60;
+let timerInterval;
 
-      if (counter < 0) {
-        clearInterval(timerInterval);
-        document.getElementById("quiz").remove();
-        loadScore();
-      }
-    }, 1000);
+var startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", function () {
+  document.getElementById("start-screen").style.display = "none";
+  document.getElementById("quiz").style.display = "flex";
+  timer(seconds);
+});
+
+function displayQuestion() {
+  const question = document.getElementById("question");
+  const quiz = document.getElementById("answers");
+
+  question.innerHTML = Questions[currentQuestion].q;
+  answers.innerHTML = ""; // clears out previous answers
+
+  for (let i = 0; i < Questions[currentQuestion].a.length; i++) {
+    const answerButton = document.createElement("button");
+    answerButton.textContent = Questions[currentQuestion].a[i].text;
+
+    answerButton.addEventListener("click", function () {
+      checkAnswer(i);
+    });
+    quiz.appendChild(answerButton);
   }
-  
-  function decrementTimer() {
-    seconds -= 5;
-    if (seconds < 0) {
-      seconds = 0;
+}
+displayQuestion();
+
+function loadScore() {
+  const scoreDiv = document.getElementById("score");
+  const finalScore = document.getElementById("final-score");
+  scoreDiv.style.display = "flex";
+  scoreMessage = `${score} out of ${Questions.length}`;
+  finalScore.innerHTML = scoreMessage;
+}
+
+function highScore() {
+  const highScoreDiv = document.getElementById("high-score");
+  const highScore = document.getElementById("high-score");
+}
+
+function timer(seconds) {
+  const timerDiv = document.getElementById("timer");
+  let counter = seconds;
+
+  timerInterval = setInterval(() => {
+    timerDiv.innerText = counter;
+    counter--;
+    if (counter < 10) {
+      document.getElementById("timer").style.color = "red";
     }
-  }
-  
-  function nextQuestion() {
-    if (currentQuestion < Questions.length - 1) {
-      currentQuestion++;
-      displayQuestion();
-    } else {
+
+    if (counter < 0) {
+      clearInterval(timerInterval);
       document.getElementById("quiz").remove();
       loadScore();
     }
+  }, 1000);
+}
+
+function decrementTimer() {
+  seconds -= 5;
+  if (seconds < 0) {
+    seconds = 0;
   }
-  
-  function checkAnswer(selectedIndex) {
-    if (Questions[currentQuestion].a[selectedIndex].isCorrect) {
-      score++;
-      console.log("Correct");
-    } else {
-      console.log("Incorrect");
-      decrementTimer(); // Decrement the timer by 5 seconds
-    }
-  
-    nextQuestion();
+}
+
+function nextQuestion() {
+  if (currentQuestion < Questions.length - 1) {
+    currentQuestion++;
+    displayQuestion();
+  } else {
+    document.getElementById("quiz").remove();
+    loadScore();
   }
-  
-  let submitButton = document.getElementById("submit-score");
-  submitButton.addEventListener("click", function () {
-    const initials = document.getElementById("initials").value;
-    const scoreObj = {
-      initials: initials,
-      score: score,
-    };
-  
-    let highScores = localStorage.setItem("highScores", JSON.stringify) || [];
-    highScores.push(scoreObj);
-    JSON.parse(localStorage.getItem("highScores")
+}
+
+function checkAnswer(selectedIndex) {
+  if (Questions[currentQuestion].a[selectedIndex].isCorrect) {
+    score++;
+    console.log("Correct");
+  } else {
+    console.log("Incorrect");
+    decrementTimer(); // Decrement the timer by 5 seconds
+  }
+
+  nextQuestion();
+}
+
+let submitButton = document.getElementById("submit-score");
+submitButton.addEventListener("click", function () {
+  const initials = document.getElementById("initials").value;
+  const scoreObj = {
+    initials: initials,
+    score: score,
+  };
+
+  let highScores = localStorage.setItem("highScores", JSON.stringify) || [];
+  highScores.push(scoreObj);
+  JSON.parse(localStorage.getItem("highScores")
     (highScores));
-  
-    window.location.href = "highscores.html";
+
+  window.location.href = "highscores.html";
 });
